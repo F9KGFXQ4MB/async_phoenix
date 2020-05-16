a888ffe8fb613a303dde29e15a1910b1ce51606b
user <user@example.com>
2020-05-16 14:55:17 +0200

    initial commit

---
+++ lib/myapp_web/live/product_live/index.ex
@@ -0,0 +1,59 @@
+defmodule MyappWeb.ProductLive.Index do
+  use MyappWeb, :live_view
+
+  alias Myapp.Products
+  alias Myapp.Products.Product
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, :products, fetch_products())}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :edit, %{"id" => id}) do
+    socket
+    |> assign(:page_title, "Edit Product")
+    |> assign(:product, Products.get_product!(id))
+  end
+
+  defp apply_action(socket, :new, _params) do
+    socket
+    |> assign(:page_title, "New Product")
+    |> assign(:product, %Product{})
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, "Listing Products")
+    |> assign(:product, nil)
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    product = Products.get_product!(id)
+    {:ok, _} = Products.delete_product(product)
+
+    {:noreply, assign(socket, :products, fetch_products())}
+  end
+
+  def handle_info({_, result}, socket) do
+    require Logger
+    Logger.error("Received")
+
+    send_update(FormComponent, id: "product-form", refreshing: false)
+    {:noreply, socket}
+  end
+
+  def handle_info({:DOWN, _, :process, _,  :normal}, socket) do
+    {:noreply, socket}
+  end
+
+
+  defp fetch_products do
+    Products.list_products()
+  end
+end
